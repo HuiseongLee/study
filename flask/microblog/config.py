2f69143173980df261ca3326da3f0ea594a491c0
HuiSeong Lee <huiseong.lee@vuno.co>
2021-06-02 16:08:57 +0900

    Chapter 9: Pagination

--- flask/microblog/config.py
+++ flask/microblog/config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['huiseong.lee@vuno.co']
+    POSTS_PER_PAGE = 3
