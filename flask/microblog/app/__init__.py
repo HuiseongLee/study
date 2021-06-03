e48bcf9710a5895f49219fb06587b183ac8c4d8e
HuiSeong Lee <huiseong.lee@vuno.co>
2021-06-03 15:45:10 +0900

    Chapter 11: Facelift

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 import logging
 import os
@@ -16,8 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
-
-from app import routes, models, errors
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -47,3 +47,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+from app import routes, models, errors
